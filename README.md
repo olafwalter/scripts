8151b0b5dd127ffeb99968bd6abb1912451edeb6
Olaf Walter <97017969+olafwalter@users.noreply.github.com>
2022-06-22 17:46:22 +0200

    updated README, new location for Backups

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # snippets
 Snippets and scripts for daily work
+
+- backup-macOS-home.sh creates a folder on your OneDrive with a backup of some files and folders. Make sure to check the location of your Onedrive and other settings.
+- unsafelink.py decodes Outlook's "safe links" so that you can review them before you click on the link.
--- backup-macOS-home.sh
+++ backup-macOS-home.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 # backup-macOS-home.sh
 
+ISODATE=$(date -u +"%Y%m%dT%H%M%SZ") 
+DESTINATION="$HOME/OneDrive/OneDrive - Bayer/Backup/$ISODATE"
+
 FILES=(
     'Library/Application Support/Google/Chrome/Default/Bookmarks.bak'
     '.zshrc'
@@ -13,9 +16,6 @@ FOLDERS=(
     'Documents'
 )
 
-ISODATE=$(date -u +"%Y%m%dT%H%M%SZ") 
-DESTINATION=$HOME/Backup/$ISODATE
-
 [[ -n $DESTINATION ]] && mkdir -pv $DESTINATION
 if [[ ! -d $DESTINATION ]]
 then
@@ -23,20 +23,10 @@ then
     exit -1
 fi
 
+echo "Backing up to $DESTINATION..."
 exec &> ${DESTINATION}/${0}.log
 
 cd $HOME
-echo "Zipping folders..."
-for i in $FOLDERS; do
-    if [[ -d $i ]]
-    then
-        # tar cvzf ${DESTINATION}.tar.gz $i
-        zip -r ${DESTINATION}/${i}.zip $i
-    else
-        echo "Error: Directory $i is not readable."
-        exit -1
-    fi
-done
 
 echo "Copying files..."
 for i in $FILES; do
@@ -45,6 +35,18 @@ for i in $FILES; do
         cp -v $i $DESTINATION
     else
         echo "Error: File $i is not readable."
+        exit -1
+    fi
+done
+
+echo "Zipping folders..."
+for i in $FOLDERS; do
+    if [[ -d $i ]]
+    then
+        # tar cvzf ${DESTINATION}.tar.gz $i
+        zip -r ${DESTINATION}/${i}.zip $i
+    else
+        echo "Error: Directory $i is not readable."
         exit -1
     fi
 done
